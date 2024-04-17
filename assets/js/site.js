100ab277f9732180d27c6327c1c5550b90b6dea4
JustDondo <morten1-18@hotmail.com>
2024-04-17 08:23:37 +0200

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,14 @@
 // write cool JS here!!
+console.group('konsol opgave')
+console.log('Jeg er en almindelig konsol log')
+console.log('Jeg er en konsol info: en variabel')
+console.groupEnd()
+
+console.warn('Jeg er en advarelse')
+console.error('Jeg er en fejl')
+
+console.time()
+
 const myInfo = 'en variabel';
 const myBooks = ["The Catcher in the Rye",
     "To Kill a Mockingbird",
@@ -11,3 +21,6 @@ const myBooks = ["The Catcher in the Rye",
     "The Da Vinci Code",
     "The Hunger Games"]
 
+console.table(["The Catcher in the Rye", "To kill a Mockingbird", "1984", "Pride and Pejudice", "The Great Gatsby", "The Hobbit", "Harry Potter and the Sorcerer's Stone", "The Lord of the Rings", "The Da Vinci Code", "The Hunger Games"])
+
+console.timeEnd()
